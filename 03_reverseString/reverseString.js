99573788963e8f118d2331e79d14822d9b975d6f
Didi nGon- <ngon-@hotmail.com>
2022-08-22 20:26:30 +0100

    Complete ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let out = '';
+    for(char of string){
+        out = char + out;
+    }
+    return out;
 };
 
 // Do not edit below this line
