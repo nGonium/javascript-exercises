3d94e4f066bc0d3b0d0b6c17dfc541bf47116597
Didi nGon- <ngon-@hotmail.com>
2022-08-22 20:56:06 +0100

    Complete ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(src, ...removeElements) {
+    for (i in src) {
+        // Using while-loop to avoid for-loop skipping elements after deletion
+        while (removeElements.includes(src[i])) {
+            src.splice(i, 1);
+        } 
+    }
+    return src;
 };
+
+// Using higher order function but creates a new array instead of mdodifying the original
+const removeFromArrayWithFilter = function (src, ...removeElements) {
+    return src.filter(el => !removeElements.includes(el));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
