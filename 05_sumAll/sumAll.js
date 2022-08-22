7eb15bb3ccec48b9cf187fd7e19b3da67bef58c0
Didi nGon- <ngon-@hotmail.com>
2022-08-22 21:13:22 +0100

    Complete ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if(int1 < 0 || typeof int1 != 'number' 
+    || int2 < 0 || typeof int2 != 'number') {
+        return 'ERROR';
+    }
+    let sum = 0;
+    const [start, end] = int1 < int2 ? [int1, int2] : [int2, int1]
+    for (let current = start; current <= end; current++) {
+        sum += current;
+    }
+    return sum;
 };
 
 // Do not edit below this line
