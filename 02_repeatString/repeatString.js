d66930a312a7f5bbb5b646d1730d2a70fd20e134
Didi nGon- <ngon-@hotmail.com>
2022-08-22 20:22:56 +0100

    Complete ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0)  return 'ERROR'
+    let out = '';
+    for(let rep = 1; rep <= num; rep++) {
+        out += string;
+    } 
+    return out;
 };
 
 // Do not edit below this line
